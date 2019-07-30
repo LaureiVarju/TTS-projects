2c31f0db7a90b5f3d224f2befedfd38e523170e2
unknown <mdilzell.com>
2019-07-30 14:41:41 -0400

    gotta push these

--- week2Projects/monksite/javascript.js
+++ week2Projects/monksite/javascript.js
@@ -1,3 +1,25 @@
+
 $(document).ready(function(){
   $('.header').height($(window).height());
-})
+});
+
+/*
+$("#burton button").on("click", function() {
+  $("button").hide();
+
+};
+/*
+$("#burton button").on("click", function(){
+
+
+$("#burton").hide();
+
+});
+
+/*
+function myFunction() {
+  document.getElementById("burton").style.display = "none";
+}
+
+*/
+
