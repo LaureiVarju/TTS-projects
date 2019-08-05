136102cd56c83c3a55714ad071a58704628ad8f9
unknown <mdilzell.com>
2019-08-05 11:19:10 -0400

    Cleaned up file trees

--- JavaScript/Madlib.js
+++
@@ -1,9 +0,0 @@
-alert("Please enter the following...");
-var excl = prompt("Exclamation");
-var name = prompt("Name");
-var verb = prompt("Verb");
-console.log(excl + "!");
-
-
-console.log(excl + "! Is that " + name + "?" + " Wow, they sure do love to " + verb +
-"!");
---
+++ week_1_projects/JavaScript_exercises/Madlib.js
@@ -0,0 +1,9 @@
+alert("Please enter the following...");
+var excl = prompt("Exclamation");
+var name = prompt("Name");
+var verb = prompt("Verb");
+console.log(excl + "!");
+
+
+console.log(excl + "! Is that " + name + "?" + " Wow, they sure do love to " + verb +
+"!");
